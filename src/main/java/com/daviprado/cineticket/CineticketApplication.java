e22fc4d75119e6c20c49b59f7daa508a11f6e5d1
davi prado <pradodavi12@gmail.com>
2026-07-17 16:23:56 -0300

    primeiro commit

---
+++ src/main/java/com/daviprado/cineticket/CineticketApplication.java
@@ -0,0 +1,13 @@
+package com.daviprado.cineticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daviprado/cineticket/CineticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daviprado.cineticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
